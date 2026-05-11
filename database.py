f74fb34a39b2fc8a77d60308bbb787a11623ac39
alsraybaqr589-boop <alsraybaqr589@gmail.com>
2026-05-11 23:17:56 +0300

    Update database.py

--- database.py
+++ database.py
@@ -1,1 +1,29 @@
+import sqlite3
 
+conn = sqlite3.connect("roulette.db")
+
+cur = conn.cursor()
+
+# جدول الروليتات
+cur.execute('''
+CREATE TABLE IF NOT EXISTS roulettes(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT,
+    max_users INTEGER,
+    winners INTEGER,
+    channel TEXT,
+    creator INTEGER,
+    type TEXT
+)
+''')
+
+# جدول المشاركين
+cur.execute('''
+CREATE TABLE IF NOT EXISTS participants(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    roulette_id INTEGER,
+    user_id INTEGER
+)
+''')
+
+conn.commit()
