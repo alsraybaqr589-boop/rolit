31d39b42ef3566c93dea08332c461f1dd7b05529
alsraybaqr589-boop <alsraybaqr589@gmail.com>
2026-05-11 23:18:51 +0300

    Update states.py

--- states.py
+++ states.py
@@ -1,1 +1,11 @@
+from aiogram.fsm.state import StatesGroup, State
 
+class CreateRoulette(StatesGroup):
+
+    title = State()
+
+    max_users = State()
+
+    winners = State()
+
+    channel = State()
