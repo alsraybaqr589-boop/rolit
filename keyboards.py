a118377dc910725fb91ee69c87de1f3a5b38cf71
alsraybaqr589-boop <alsraybaqr589@gmail.com>
2026-05-11 23:22:27 +0300

    Update keyboards.py

--- keyboards.py
+++ keyboards.py
@@ -1,1 +1,78 @@
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
+# القائمة الرئيسية
+main_menu = InlineKeyboardMarkup(
+    inline_keyboard=[
+
+        [
+            InlineKeyboardButton(
+                text="🌐 روليت عادي",
+                callback_data="normal"
+            )
+        ],
+
+        [
+            InlineKeyboardButton(
+                text="📜 روليت أحكام",
+                callback_data="rules"
+            )
+        ],
+
+        [
+            InlineKeyboardButton(
+                text="🌈 روليت مميز",
+                callback_data="vip"
+            )
+        ],
+
+        [
+            InlineKeyboardButton(
+                text="📢 قناتنا",
+                url="https://t.me/NQJNQ"
+            )
+        ]
+
+    ]
+)
+
+# قائمة الروليت المميز
+vip_menu = InlineKeyboardMarkup(
+    inline_keyboard=[
+
+        [
+            InlineKeyboardButton(
+                text="🌈 بدء إنشاء روليت",
+                callback_data="create_vip"
+            )
+        ],
+
+        [
+            InlineKeyboardButton(
+                text="⚙️ إعدادات القنوات",
+                callback_data="channels"
+            )
+        ],
+
+        [
+            InlineKeyboardButton(
+                text="🏠 العودة للقائمة",
+                callback_data="back"
+            )
+        ]
+
+    ]
+)
+
+# زر الإلغاء
+cancel_button = InlineKeyboardMarkup(
+    inline_keyboard=[
+
+        [
+            InlineKeyboardButton(
+                text="❌ إلغاء",
+                callback_data="cancel"
+            )
+        ]
+
+    ]
+)
