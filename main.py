bed94f97b833d76c6a9a04ffeaf98b1e82c15ff5
alsraybaqr589-boop <alsraybaqr589@gmail.com>
2026-05-13 02:22:42 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import telebot
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-TOKEN = "NQJNQ"
+TOKEN = "8735268386:AAGzFCX4yKoTjdgSjbFId1xP4Rhc-BGJ9oo"
 
 bot = telebot.TeleBot(TOKEN)
 
