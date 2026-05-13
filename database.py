001c77883d283b4835aa665b6c6c1cd95c067b21
alsraybaqr589-boop <alsraybaqr589@gmail.com>
2026-05-13 19:56:01 +0300

    Update database.py

--- database.py
+++ database.py
@@ -1,1 +1,24 @@
+import aiosqlite
 
+DB_NAME = "roulette.db"
+
+async def setup_db():
+    async with aiosqlite.connect(DB_NAME) as db:
+        await db.execute('''
+        CREATE TABLE IF NOT EXISTS roulettes (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            type TEXT,
+            chat_id INTEGER,
+            message_id INTEGER
+        )
+        ''')
+
+        await db.execute('''
+        CREATE TABLE IF NOT EXISTS participants (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            roulette_id INTEGER,
+            user_id INTEGER
+        )
+        ''')
+
+        await db.commit()
