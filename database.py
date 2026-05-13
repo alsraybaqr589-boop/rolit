b556e68c4ddb17b65b029b9d2b182c0b7b1d3cbd
alsraybaqr589-boop <alsraybaqr589@gmail.com>
2026-05-13 19:38:45 +0300

    Update database.py

--- database.py
+++ database.py
@@ -1,18 +1,1 @@
-import aiosqlite
 
-DB_NAME = "rolit.db"
-
-
-async def create_db():
-    async with aiosqlite.connect(DB_NAME) as db:
-
-        await db.execute("""
-        CREATE TABLE IF NOT EXISTS roulettes (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            creator_id INTEGER,
-            title TEXT,
-            players TEXT
-        )
-        """)
-
-        await db.commit()
